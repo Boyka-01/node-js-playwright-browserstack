1c6b5ef1b5b6a4bb1bd1949d22350c7cc4407cb9
Boyka-01 <114407684+Boyka-01@users.noreply.github.com>
2024-07-22 11:54:32 +0500

    test

--- tests/bstack_local_test.js
+++ tests/bstack_local_test.js
@@ -1,22 +1,7 @@
-const { test, expect } = require('@playwright/test');
-
-test('Sample Test', async ({ page }) => {
-    await page.goto('https://opticommneud01stvnjnsapp.azurewebsites.net/');
-
-    const cookies = await page.context().cookies();
-    console.log(cookies);
-
-    await page.click('#menu-item-16983 > .ekit-menu-nav-link');
-    
-    await page.waitForTimeout(3000);
-    
-    await page.click('#snize-product-10793 > .snize-view-link > .snize-item > .snize-thumbnail-wrapper > .snize-thumbnail');
-    
-    await page.waitForSelector('.elementor-element-7905e8cb > .elementor-widget-wrap');
-    
-    await page.fill('#input_3_2', 'Opticommerce');
-    await page.fill('#input_3_4', '123456');
-    await page.fill('#input_3_3', 'formtest@opticommerce.co.uk');
-    await page.fill('#input_3_5', 'test');
-    await page.waitForTimeout(10000); // Wait for 10 seconds
+const { expect, test } = require('@playwright/test');
+const sleep = require('util').promisify(setTimeout)
+test('BStack local is working', async ({ page }) => {
+  await page.goto('http://bs-local.com:45454/');
+  const title = await page.title();
+  expect(title).toContain('BrowserStack Local');
 });
