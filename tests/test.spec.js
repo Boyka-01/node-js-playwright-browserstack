a723132eaf84b3fda3689f746a00878581ff8859
Boyka-01 <114407684+Boyka-01@users.noreply.github.com>
2024-07-22 16:58:59 +0500

    sdadsd

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,12 +1,14 @@
 const { test, expect } = require('@playwright/test');
 
 test('Sample Test', async ({ page }) => {
-    await page.goto('https://opticommneud01stvnjnsapp.azurewebsites.net/');
-
+    await page.goto('https://opticommneud01stvnjnsapp.azurewebsites.net/', { timeout: 60000 }); // Increase timeout to 60 seconds
+    await page.waitForTimeout(5000); // Increase timeout to 5 seconds
+    
     const cookies = await page.context().cookies();
     console.log(cookies);
 
-    await page.click('#menu-item-16983 > .ekit-menu-nav-link');
+    //await page.waitForSelector('#menu-item-16983 > .ekit-menu-nav-link', { state: 'visible' });
+    //await page.click('#menu-item-16983 > .ekit-menu-nav-link');
     
     await page.waitForTimeout(3000);
     
