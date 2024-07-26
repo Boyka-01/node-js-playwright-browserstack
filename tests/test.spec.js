f50bce40adfa09353cdfab62a0de6315cec24444
Boyka-01 <114407684+Boyka-01@users.noreply.github.com>
2024-07-26 16:36:36 +0500

    code

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -13,18 +13,19 @@ test('Complete Form Submission Flow', async ({ page }) => {
         await page.screenshot({ path: screenshotPath });
     };
 
-
+    // Navigate to the initial page
     await page.goto('https://opticommneud01stvnjnsapp.azurewebsites.net/', { timeout: 60000 });
     console.log('Current URL:', page.url());
     await saveScreenshot(page, 'home');
 
-   
+    // Basic Test with Assertions
     await page.waitForSelector('#menu-item-16983 > .ekit-menu-nav-link');
     await expect(page.locator('#menu-item-16983 > .ekit-menu-nav-link')).toBeVisible();
     await page.click('#menu-item-16983 > .ekit-menu-nav-link');
+    console.log('Current URL:', page.url());
     await saveScreenshot(page, 'menu');
 
-   
+    // Selecting the product from PLP
     await page.waitForSelector('#snize-product-10793 > .snize-view-link > .snize-item > .snize-thumbnail-wrapper > .snize-thumbnail');
     await expect(page.locator('#snize-product-10793 > .snize-view-link > .snize-item > .snize-thumbnail-wrapper > .snize-thumbnail')).toBeVisible();
     await expect(page.locator('#snize-product-10793 > .snize-view-link > .snize-item > .snize-thumbnail-wrapper > .snize-thumbnail')).toBeEnabled();
@@ -32,38 +33,32 @@ test('Complete Form Submission Flow', async ({ page }) => {
     console.log('Current URL:', page.url());
     await saveScreenshot(page, 'product');
 
-  
+    // Getting the contact us form
     await page.waitForSelector('.elementor-element-7905e8cb > .elementor-widget-wrap');
     await expect(page.locator('.elementor-element-7905e8cb > .elementor-widget-wrap')).toBeVisible();
     console.log('Current URL:', page.url());
     await saveScreenshot(page, 'contact-form');
 
-    
+    // Filling in the form value
     await expect(page.locator('#input_3_2')).toBeEnabled();
     await page.fill('#input_3_2', formData.name);
     await expect(page.locator('#input_3_2')).toHaveValue(formData.name);
-
     await expect(page.locator('#input_3_4')).toBeEnabled();
     await page.fill('#input_3_4', formData.telephone);
     await expect(page.locator('#input_3_4')).toHaveValue(formData.telephone);
-
     await expect(page.locator('#input_3_3')).toBeEnabled();
     await page.fill('#input_3_3', formData.email);
     await expect(page.locator('#input_3_3')).toHaveValue(formData.email);
-
     await expect(page.locator('#input_3_5')).toBeEnabled();
     await page.fill('#input_3_5', formData.message);
     await expect(page.locator('#input_3_5')).toHaveValue(formData.message);
     await saveScreenshot(page, 'form-filled');
-
- 
     await expect(page.locator('#gform_submit_button_3')).toBeEnabled();
     await page.click('#gform_submit_button_3');
-    await saveScreenshot(page, 'form-submitted');
-
+    // Validating the response
     try {
         const successMessageLocator = page.locator(
-            '//div[@id="gform_confirmation_message_2" and @class="gform_confirmation_message_2 gform_confirmation_message"]'
+            '//div[@id="gform_confirmation_message_3" and @class="gform_confirmation_message_3 gform_confirmation_message"]'
         );
         await successMessageLocator.waitFor({ state: 'visible', timeout: 20000 });
         const successMessage = await successMessageLocator.textContent();
@@ -71,6 +66,5 @@ test('Complete Form Submission Flow', async ({ page }) => {
         await saveScreenshot(page, 'response');
     } catch (error) {
         console.error('Error validating the form submission response:', error.message);
-        await saveScreenshot(page, 'response-failed');
     }
 });
